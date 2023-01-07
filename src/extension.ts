83b9ad977bf308067f5a870b7089374040b906fb
Mike Hodnick <mike@kindohm.com>
2023-01-07 10:09:50 -0600

    clean up bootup

--- src/extension.ts
+++ src/extension.ts
@@ -15,12 +15,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	log('tcpure-receiver is active');
-	startServer(resourocePath);
+	let booted = false;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tcpure-receiver.boot', () => { });
+	let disposable = vscode.commands.registerCommand('tcpure-receiver.boot', () => {
+		if (booted) {
+			return;
+		}
+		startServer(resourocePath);
+		booted = true;
+		vscode.window.setStatusBarMessage("tcpure-receiver booted");
+
+	});
 
 	context.subscriptions.push(disposable);
 }
