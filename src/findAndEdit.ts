b5418fed6c39186b192a8dfd203e160fdfaa54f8
Mike Hodnick <mike@kindohm.com>
2023-01-07 11:21:43 -0600

    remove commented code

--- src/findAndEdit.ts
+++ src/findAndEdit.ts
@@ -30,22 +30,6 @@ export const findAndEdit = ({ control, value }: { control: string, value: number
     return newEdits.concat(controlEdits);
   }, initialEdits);
 
-  // const edits = getEditsForControl(control, lines, document, editor);
-
-
-
-  // const targetLineNumbers = lines
-  //   .map((l, i) => ({ line: l, lineNumber: i }))
-  //   .filter(l => l.line.includes(`//${control}`))
-  //   .map(l => l.lineNumber);
-
-  // if (targetLineNumbers.length === 0) {
-  //   console.warn('did not find any target lines', control);
-  //   return;
-  // }
-
-  // const edits = targetLineNumbers.map(n => getReplacement(n, control, document, editor));
-
   editor.edit((editBuilder) => {
     edits.filter(e => !!e).forEach(e => {
       if (!!e) {
